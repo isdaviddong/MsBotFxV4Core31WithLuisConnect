fb55d4e50c873801f4bdcb706161d68debd30120
twdeveloper <twdeveloper@outlook.com>
2021-06-03 15:18:46 +0800

    v1

---
+++ EchoBot1/Bots/EchoBot.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.13.2
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace EchoBot1.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var luisResult = LUIS.MakeRequest(turnContext.Activity.Text);
+            var replyText = $"您說了 :  {turnContext.Activity.Text}";
+
+            foreach (var item in luisResult.entities)
+            {
+                replyText += $"{System.Environment.NewLine} entity: {item.entity} - {item.score}";
+            }
+            foreach (var item in luisResult.intents)
+            {
+                replyText += $"{System.Environment.NewLine} intent: {item.intent} - {item.score}";
+            }
+            replyText += $"{System.Environment.NewLine} topScoringIntent: {luisResult.topScoringIntent.intent} - {luisResult.topScoringIntent.score}";
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
---
+++ EchoBot1/Bots/LUIS.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EchoBot1.Bots
+{
+    public class LUIS
+    {
+
+        const string key = "55baabf9c3384eebb9a00a6c5f026f7c";
+        const string endpoint = "westus.api.cognitive.microsoft.com";
+        const string appId = "313a5502-498e-4879-912d-b7db33fe4d4f";
+
+        public static LUISResult MakeRequest(string utterance)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            var endpointUri = String.Format(
+                "https://{0}/luis/v2.0/apps/{1}?verbose=true&timezoneOffset=0&subscription-key={3}&q={2}",
+                endpoint, appId, utterance, key);
+
+            var response = client.GetAsync(endpointUri).Result;
+
+            var strResponseContent = response.Content.ReadAsStringAsync().Result;
+            var Result = Newtonsoft.Json.JsonConvert.DeserializeObject<LUISResult>(strResponseContent);
+            // Display the JSON result from LUIS
+            return Result;
+        }
+    }
+
+    #region "LUIS Model"
+
+    public class TopScoringIntent
+    {
+        public string intent { get; set; }
+        public double score { get; set; }
+    }
+
+    public class Intent
+    {
+        public string intent { get; set; }
+        public double score { get; set; }
+    }
+
+    public class Resolution
+    {
+        public string value { get; set; }
+    }
+
+    public class Entity
+    {
+        public string entity { get; set; }
+        public string type { get; set; }
+        public int startIndex { get; set; }
+        public int endIndex { get; set; }
+        public double score { get; set; }
+        public Resolution resolution { get; set; }
+    }
+
+    public class LUISResult
+    {
+        public string query { get; set; }
+        public TopScoringIntent topScoringIntent { get; set; }
+        public List<Intent> intents { get; set; }
+        public List<Entity> entities { get; set; }
+    }
+    #endregion
+}
